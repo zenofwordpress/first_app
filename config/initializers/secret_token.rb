6553203c819be6d1bdfc7c4e2ae28d58b4c4773a
zenofwordpress <gturner@zenofwp.com>
2014-03-04 16:30:14 -0800

    init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '0a424fe3e5085373531b0e270978af73a014a77e88c783fd947cfb1c64867362e5aaf4fc3e6d35657bc6677430a137f6a84b184572413255c739a85f2f9d8983'
